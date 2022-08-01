e71a4ff3d98afe70c9491c23628774bf10e00d65
eva <evafdezarroyo@gmail.com>
2022-08-01 19:30:17 +0200

    lista

---
+++ carpetaEjercicios/lista.js
@@ -0,0 +1,13 @@
+const list = [
+    `Eva`,
+    36,
+    false,
+    new birthDay (12/03/86),
+    {
+        title : `El temor de un hombre sabio`,
+        autor : `Patrick Rothfuss`,
+        fecha : new data  (01/02/2015),
+        url : `http://lahistoriadekvothe.com/`
+    }
+
+]
